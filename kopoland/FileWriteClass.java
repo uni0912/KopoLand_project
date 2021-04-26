b7c151e1d7e24a9dffee621d46e31a4cca0c6fcb
uni0912 <cwyun0912@gmail.com>
2021-04-26 10:38:58 +0900

    make classes

---
+++ kopoland/FileWriteClass.java
@@ -0,0 +1,5 @@
+package kopoland;
+
+public class FileWriteClass {
+
+}
---
+++ kopoland/KopoMain.java
@@ -0,0 +1,10 @@
+package kopoland;
+
+public class KopoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
